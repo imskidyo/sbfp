4ce526d2f2a1d258bf4ef2d950a9a9be83f04217
imskidyo <imskidyo@gmail.com>
2024-07-16 12:49:04 +0300

    Main program

---
+++ sbfp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+from string import printable
+import time
+import os
+import webbrowser
+clear = lambda : os.system('cls')
+
+print("Coded by imskidyo")
+print("Discord Username: imskidyo")
+print("Please wait 20 seconds!")
+webbrowser.open('https://www.youtube.com/@imskidyo?sub_confirmation=1')
+webbrowser.open('https://discord.gg/YEXg6Un2Dp')
+time.sleep(20)
+clear()
+for i1 in printable:
+    for i2 in printable:
+        for i3 in printable:
+            for i4 in printable:
+                print(i1+i2+i3+i4)
+# Coded by imskidyo
